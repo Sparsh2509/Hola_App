296fdb7ad20bd9779a6da7a9ec57643e9ad8539e
Sparsh2509 <sparshgupta250904@gmail.com>
2024-11-29 21:35:41 +0530

    Home page ui changes started

---
+++ lib/models/homepage_model.dart
@@ -0,0 +1,21 @@
+class HomepageModel{
+      String image;
+      String userName;
+      String caption;
+      String imageUrl;
+      int likes;
+      int comments;
+      bool isliked;
+
+HomepageModel({
+  required this.image,
+  required this.userName,
+  required this.caption,
+  required this.imageUrl,
+  required this.likes,
+  required this.comments,
+  required this.isliked
+
+  
+});
+}
