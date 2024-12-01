ec65af9a93f17b319adf1bac30842074f2d8429c
Sparsh2509 <sparshgupta250904@gmail.com>
2024-12-01 21:20:07 +0530

    Post page ui started

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -7,5 +7,6 @@ const iconColor = Color(0xffBBACF2);
 const buttonColor = Color(0xff6C60A0);
 const blackColor = Colors.black;
 const homeAppBarColor = Color(0xFF1C1C30);
+const blueColor = Colors.blue;
 
 // const hyperLinkColor = Colors.purple;
--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+import 'package:hola_app/constants/colors.dart';
+import 'package:hola_app/constants/size.dart';
 
 class Post extends StatefulWidget {
   const Post({super.key});
@@ -10,6 +13,44 @@ class Post extends StatefulWidget {
 class _PostState extends State<Post> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Cancel",style: TextStyle(color: whiteColor,fontSize: 20),),
+                    Row(
+                      children: [
+                        Text("Recent",style: TextStyle(color: whiteColor,fontSize: 20)),
+                        Icon(Icons.keyboard_arrow_down,color: whiteColor,),
+                      ],
+                    ),
+                    Text("Next",style: TextStyle(color: blueColor,fontSize: 20))
+                   
+                
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: screenHeight*0.02,
+              ),
+              Container(
+                height: screenHeight*0.4,
+                width: screenWidth,
+                decoration: BoxDecoration(
+                  border: Border.all(color: iconColor),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
