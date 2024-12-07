c84ea255c2c2c74ddff7279e9ff9c836bb99475b
Sparsh2509 <sparshgupta250904@gmail.com>
2024-12-07 23:17:37 +0530

    Post page ui changed

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hola_app/constants/colors.dart';
+import 'package:hola_app/pages/post.dart';
 
 class Location extends StatefulWidget {
   const Location({super.key});
@@ -8,8 +10,85 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+  final List<Map<String, dynamic>> posts = [];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            'Your Posts',
+            style: TextStyle(fontSize: 15,color: whiteColor)
+          ),
+          centerTitle: true,
+          leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: whiteColor),
+          onPressed: () {
+            Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (BuildContext context) => Post()));
+          },
+
+        ),
+        ),
+        body: Expanded(
+          child: ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              return Card(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                color: greyColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (posts[index]['image'] != null)
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.file(
+                            posts[index]['image'],
+                            height: 150,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      const SizedBox(height: 8),
+                      if (posts[index]['caption'] != null &&
+                          posts[index]['caption'].isNotEmpty)
+                        Text(posts[index]['caption'],
+                            style: TextStyle(fontSize: 15)),
+                      const SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            posts[index]['text'],
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: whiteColor,
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.delete,
+                                color: iconColor),
+                            onPressed: () {
+                              setState(() {
+                                posts.removeAt(index);
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,7 +1,9 @@
 
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:hola_app/constants/colors.dart';
-import 'package:hola_app/constants/size.dart';
+import 'package:hola_app/pages/location.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Post extends StatefulWidget {
   const Post({super.key});
@@ -11,46 +13,170 @@ class Post extends StatefulWidget {
 }
 
 class _PostState extends State<Post> {
+  final TextEditingController postController = TextEditingController();
+  final TextEditingController captionController = TextEditingController();
+   final List<Map<String, dynamic>> posts = [];
+
+  File? selectedImage;
+  final picker = ImagePicker();
+  
+  Future getImageGallery() async{
+    final PickedFile =await picker.pickImage(
+      source: ImageSource.gallery ,
+      imageQuality: 80,
+      );
+      setState(() {
+        if(PickedFile != null){
+          selectedImage = File(PickedFile.path);
+        }
+        else{
+          print(" NO Image picked");
+        }
+      });
+  }
+  // Future<void> pickImage() async {
+  //   final pickedFile =
+  //       await ImagePicker().pickImage(source: ImageSource.gallery);
+  //   if (pickedFile != null) {
+  //     setState(() {
+  //       selectedImage = File(pickedFile.path);
+  //     });
+  //   }
+  // }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
+        // body: SingleChildScrollView(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+        title: Text(
+          
+          'CREATE POST',
+          style: TextStyle(fontSize: 15,color: whiteColor)
+        ),
+        centerTitle: true,
+        // leading: IconButton(
+        //     onPressed: () {
+        //       Navigator.pushReplacement(context,
+        //           MaterialPageRoute(builder: (context) =>Landing2()));
+        //     },
+        //     icon: const Icon(Icons.arrow_back)),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Cancel",style: TextStyle(color: whiteColor,fontSize: 20),),
-                    Row(
-                      children: [
-                        Text("Recent",style: TextStyle(color: whiteColor,fontSize: 20)),
-                        Icon(Icons.keyboard_arrow_down,color: whiteColor,),
-                      ],
+              TextField(
+                style: TextStyle(color: iconColor),
+                controller: captionController,
+                maxLines: 2,
+                decoration: const InputDecoration(
+                  hintText: 'Add a caption...',hintStyle: TextStyle(color: iconColor),
+                ),
+              ),
+              const SizedBox(height: 20),
+              selectedImage != null
+                  ? ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.file(
+                        selectedImage!,
+                        height: 200,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  : Container(
+                      height: 200,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: greyColor),
+                      child: Center(
+                        child: Text(
+                          'Select a Image',
+                          style: TextStyle(fontSize: 15)
+                        ),
+                      ),
                     ),
-                    Text("Next",style: TextStyle(color: blueColor,fontSize: 20))
-                   
-                
-                  ],
+              TextButton.icon(
+                onPressed: getImageGallery,
+                icon: const Icon(Icons.image),
+                label: const Text('Upload Image'),
+              ),
+              const SizedBox(height: 10),
+              // Post Content TextField
+              TextField(
+                style: TextStyle(color: iconColor),
+                controller: postController,
+                maxLines: 2,
+                decoration: const InputDecoration(
+                  hintText: 'What\'s on your mind?',hintStyle: TextStyle(color: iconColor),
                 ),
               ),
-              SizedBox(
-                height: screenHeight*0.02,
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (postController.text.isNotEmpty || selectedImage != null) {
+                    setState(
+                      () {
+                        posts.add({
+                          'text': postController.text,
+                          'caption': captionController.text,
+                          'image': selectedImage,
+                        });
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Location()));
+                      },
+                    );
+                    postController.clear();
+                    captionController.clear();
+                    selectedImage = null;
+                  }
+                },
+                child: const Text('Post'),
               ),
-              Container(
-                height: screenHeight*0.4,
-                width: screenWidth,
-                decoration: BoxDecoration(
-                  border: Border.all(color: iconColor),
-                  borderRadius: BorderRadius.circular(10)
-                ),
-              )
             ],
           ),
         ),
-      )
-    );
+      ),
+    ));
   }
 }
+          // child: Column(
+          //   children: [
+          //     Padding(
+          //       padding: const EdgeInsets.all(8.0),
+          //       child: Row(
+          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //         children: [
+          //           Text("Cancel",style: TextStyle(color: whiteColor,fontSize: 20),),
+          //           Row(
+          //             children: [
+          //               Text("Recent",style: TextStyle(color: whiteColor,fontSize: 20)),
+          //               Icon(Icons.keyboard_arrow_down,color: whiteColor,),
+          //             ],
+          //           ),
+          //           Text("Next",style: TextStyle(color: blueColor,fontSize: 20))
+                   
+                
+          //         ],
+          //       ),
+          //     ),
+          //     SizedBox(
+          //       height: screenHeight*0.02,
+          //     ),
+          //     Container(
+          //       height: screenHeight*0.4,
+          //       width: screenWidth,
+          //       decoration: BoxDecoration(
+          //         border: Border.all(color: iconColor),
+          //         borderRadius: BorderRadius.circular(10)
+          //       ),
+          //     )
+          //   ],
+          // ),
+      
